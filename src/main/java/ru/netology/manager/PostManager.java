6ff948b3c6f1d52a662610eaf03c0b002a9cbd3d
Andrey Koryakin <football2331lm@gmail.com>
2020-10-12 19:38:32 +0300

    methods fixed

--- src/main/java/ru/netology/manager/PostManager.java
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -3,10 +3,10 @@ package ru.netology.manager;
 public class PostManager {
     private PostManager[] posts;
 
-    public PostManager[] WallSearch(int ownerId, String domain, String query, boolean owners_only, int count,int offset) {
+    public PostManager[] wallSearch(int ownerId, String domain, String query, boolean owners_only, int count,int offset) {
         return null;
     }
 
-    public void WallDelete(int ownerId, String domain, String Query, boolean owners_only, int count, int offset) {
+    public void wallDelete(int ownerId, String domain, String Query, boolean owners_only, int count, int offset) {
     }
 }
